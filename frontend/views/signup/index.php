d4a8a5575793d87f6f40aa300a515dcc93e21429
wel80 <wel80@list.ru>
2020-01-30 14:12:16 +0700

    Отдельный шаблон для страницы регистрации

--- frontend/views/signup/index.php
+++ frontend/views/signup/index.php
@@ -73,16 +73,3 @@ $this->title = 'Регистрация';
                     ActiveForm::end() ?>
                 </div>
             </section>
-
-            <div class="clipart-woman">
-                <img src="<?=Url::to("/img/clipart-woman.png")?>" width="238" height="450">
-            </div>
-            <div class="clipart-message">
-                <div class="clipart-message-text">
-                <h2>Знаете ли вы, что?</h2>
-                <p>После регистрации вам будет доступно более
-                    двух тысяч заданий из двадцати разных категорий.</p>
-                    <p>В среднем, наши исполнители зарабатывают
-                    от 500 рублей в час.</p>
-                </div>
-            </div>
